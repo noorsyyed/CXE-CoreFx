a313bbaf188b22bfc7693418362a60783e17e39b
Noor <Noor.Syed@columbusglobal.com>
2025-09-29 14:13:34 +0530

    Add lightweight DI container to manage dependencies
    - ✨ Added `DataverseContainer.cs` to implement `IDataverseContainer` for service registration and instance management.
    - 🏷️ Introduced `Lifetime.cs` to define service lifetimes (`Scoped`, `Singleton`, `Transient`).
    - 🔧 Added `IDataverseContainer.cs` interface for DI container methods.
    - 🛠️ Created `Registration.cs` to encapsulate service registration details with validation and equality checks.
    - 📂 Updated `CXE.CoreFx.projitems` to include new files and `Enums\Lifetime.cs`.
    - 🗂️ Enhanced `Registration` struct with equality operators and custom hash code logic.

---
+++ CXE.CoreFx/DataverseContainer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using CXE.CoreFx.Enums;
+
+namespace CXE.CoreFx
+{
+	public class DataverseContainer : IDataverseContainer
+	{
+		private readonly Dictionary<Type, Registration> _registrations = new Dictionary<Type, Registration>();
+		private readonly ConcurrentDictionary<Type, object> _instances = new ConcurrentDictionary<Type, object>();
+
+		private IDataverseContainer AddRegistration(Type serviceType, Registration registration)
+		{
+			_registrations[serviceType] = registration;
+			return this;
+		}
+
+		public IDataverseContainer AddScoped<TService>()
+		{
+			return AddScoped(typeof(TService));
+		}
+		public IDataverseContainer AddSingleton<TService>()
+		{
+			return AddSingleton(typeof(TService));
+		}
+		public IDataverseContainer AddSingleton(Type serviceType)
+		{
+			AddRegistration(serviceType, new Registration(serviceType, Lifetime.Singleton));
+			ClearSingletonInstance(serviceType);
+			return this;
+		}
+		public IDataverseContainer AddTransient<TService>()
+		{
+			return AddTransient(typeof(TService));
+		}
+		public IDataverseContainer AddTransient(Type serviceType)
+		{
+			return AddRegistration(serviceType, new Registration(serviceType, Lifetime.Transient));
+		}
+
+		public IDataverseContainer Remove<TService>()
+		{
+			_registrations.Remove(typeof(TService));
+			return this;
+		}
+		private void ClearSingletonInstance(Type serviceType)
+		{
+			// Registration has been updated, so remove any existing instance
+			_instances.TryRemove(serviceType, out _);
+		}
+	}
+
+}
---
+++ CXE.CoreFx/Enums/Lifetime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CXE.CoreFx.Enums
+{
+	public enum Lifetime
+	{
+		/// <summary>
+		/// Type is created at most once per request, and then reused.
+		/// </summary>
+		Scoped = 1,
+		/// <summary>
+		/// Type is created at most once per container, and then reused.
+		/// </summary>
+		Singleton = 0,
+		/// <summary>
+		/// A new instance of the Type is created every time.
+		/// </summary>
+		Transient = 2,
+	}
+}
